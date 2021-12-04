cc7b460c45946a30c960722115dbf7c140da4862
Travis Helms <smasher.coder@gmail.com>
2021-12-03 23:20:23 -0600

    Add Heroku

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
